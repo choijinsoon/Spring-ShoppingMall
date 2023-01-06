0fe638d577b24fab15b4830a2c5dabf14e566c28
choijinsoon <wlstns2244@naver.com>
2023-01-06 17:07:48 +0900

    category sorting

--- src/main/java/com/myapp/shoppingmall/controller/AdminCategoryController.java
+++ src/main/java/com/myapp/shoppingmall/controller/AdminCategoryController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.myapp.shoppingmall.dao.CategoryRepository;
@@ -27,7 +29,7 @@ public class AdminCategoryController {
 
     @GetMapping
     public String index(Model model){
-        List<Category> categories = categoryRepo.findAll();
+        List<Category> categories = categoryRepo.findAllByOrderBySortingAsc();
         model.addAttribute("categories", categories);
         return "admin/categories/index";
     }
@@ -101,5 +103,22 @@ public class AdminCategoryController {
         attr.addFlashAttribute("alertClass", "alert-success");
 
         return "redirect:/admin/categories";
+    }
+
+    @PostMapping("/reorder")
+    public @ResponseBody String reorder(@RequestParam("id[]") int[] ids){
+
+        int count = 1;
+        Category category;
+
+        for(int categoryId : ids){
+            category = categoryRepo.getById(categoryId);
+            category.setSorting(count);
+
+            categoryRepo.save(category);
+            count++;
+        }
+
+        return "ok";
     }
 }
