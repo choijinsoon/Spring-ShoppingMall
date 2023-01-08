4a35579efe95651346a7608bceb3d2aeff79a26e
choijinsoon <wlstns2244@naver.com>
2023-01-08 11:46:53 +0900

    WYSIWYG 에디터 추가 (CK 에디터)

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,3 +3,16 @@ $(function () {
       if (!confirm("삭제하시겠습니까?")) return false; // 취소 시 삭제안함
     })
 });
+
+$(function () {
+  if($('#content').length){
+    ClassicEditor.create(document.querySelector('#content')).catch((error) => {
+      console.error(error);
+    })
+  }
+  if($('#description').length){
+    ClassicEditor.create(document.querySelector('#description')).catch((error) => {
+      console.log(error);
+    })
+  }
+})
